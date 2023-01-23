5b5910965f0ec5b006e47c2484d45b3a723c7991
Vahan Grigoryan <vahan.grigoryan@gmail.com>
2023-01-23 12:26:54 +0300

    -

--- src/Sites/.Bundle/21.DataWindow.js
+++ src/Sites/.Bundle/21.DataWindow.js
@@ -13,7 +13,11 @@ App.Modules.Sites.DataWindow = class extends Colibri.UI.Window {
 
         this._storages.AddHandler('SelectionChanged', (event, args) => this.__storagesSelectionChanged(event, args));
         this._data.AddHandler(['SelectionChanged', 'CheckChanged'], (event, args) => this.__dataSelectionChanged(event, args));
+        this._data.AddHandler('ColumnClicked', (event, args) => this.__clickOnDataColumn(event, args));        
+        this._data.AddHandler('ScrolledToBottom', (event, args) => this.__dataScrolledToBottom(event, args));
         this._save.AddHandler('Clicked', (event, args) => this.__saveClicked(event, args));
+
+        this._searchInput.AddHandler(['Filled', 'Cleared'], (event, args) => this.__searchInputFilled(event, args));
 
     }
 
@@ -22,6 +26,33 @@ App.Modules.Sites.DataWindow = class extends Colibri.UI.Window {
         this._publicationCallback = publicationCallback;
         this.shown = true;   
 
+    }
+
+    __clickOnDataColumn(event, args) {
+        this.__searchInputFilled(event, args);
+    }
+
+
+    __dataScrolledToBottom(event, args) {
+        const selected = this._storages.selected;
+        this._loadDataPage(selected?.tag, this._searchInput.value, this._data.sortColumn?.name, this._data.sortOrder, this._dataCurrentPage + 1);
+    }
+
+    __searchInputFilled(event, args) {
+        const selected = this._storages.selected;
+        if(!selected || (selected.tag === 'module' || selected.tag === 'group')) {
+            this._data.storage = null;
+            this._data.ClearAll(); 
+            return;           
+        }
+        
+        this._data.storage = selected.tag;
+        this._loadDataPage(selected?.tag, this._searchInput.value, this._data.sortColumn?.name, this._data.sortOrder, 1);
+    }
+
+    _loadDataPage(storage, searchTerm, sortField, sortOrder, page) {
+        this._dataCurrentPage = page;
+        Sites.LoadData(storage, searchTerm, sortField, sortOrder, page, 20);
     }
 
     __storagesSelectionChanged(event, args) {
@@ -38,14 +69,7 @@ App.Modules.Sites.DataWindow = class extends Colibri.UI.Window {
         this._data.UncheckAllRows();
         this._data.UnselectAllRows();   
         
-        if(selection.tag === 'module') {
-            return;
-        }
-
-        this._data.storage = selection.tag;
-        Sites.LoadData(this._data.storage, this._searchInput.value, this._data.sortColumn?.name, this._data.sortOrder, 1, 20, true).then((response) => {
-            this._data.value = response.result;
-        }); 
+        this.__searchInputFilled(event, args);
         
     }
 
