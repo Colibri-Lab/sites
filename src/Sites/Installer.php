08e58ba743ef2a61952abbdb15ee28e809fedc78
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2023-07-01 13:04:50 +0300

    -

--- src/Sites/Installer.php
+++ src/Sites/Installer.php
@@ -86,6 +86,7 @@ class Installer
     /**
      *
      * @param \Composer\Installer\PackageEvent $event
+     * @suppress PHP0418
      * @return void
      */
     public static function PostPackageInstall($event)
