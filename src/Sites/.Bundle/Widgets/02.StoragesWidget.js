3c12c0574376debaff58c39c071d4eb3f444987a
Vahan P. Grigoryan <vahan.grigoryan@gmail.com>
2024-03-05 15:02:05 +0300

    -

--- src/Sites/.Bundle/Widgets/02.StoragesWidget.js
+++ src/Sites/.Bundle/Widgets/02.StoragesWidget.js
@@ -38,4 +38,4 @@ App.Modules.Sites.Widgets.StoragesWidget = class extends Colibri.UI.Widget {
     }
 }
 
-try{ MainFrame.RegisterWidget('storages-stats', App.Modules.Sites.Widgets.StoragesWidget); } catch(e) {}
+try{ MainFrame && MainFrame.RegisterWidget('storages-stats', App.Modules.Sites.Widgets.StoragesWidget); } catch(e) {}
